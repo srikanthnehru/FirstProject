7f70192837f6b8c185f155a43b10fc8f0e0f68fc
Srikanth <srikanthnehrus@gmail.com>
2020-05-28 23:27:37 +0530

    feature1 add

--- regex.py
+++ regex.py
@@ -6,4 +6,9 @@ if re.search("^y.*",s,re.IGNORECASE):
     print("Agreed")
 else:
     print("not agreed")
+#feature1_branch1
+if re.search("^ok",s,re.IGNORECASE):
+    print("Agreed")
+else:
+    print("not agreed")
 
