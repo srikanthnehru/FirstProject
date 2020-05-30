ceec9c9b9b89707466cd4b66d455c524df4ddca2
Srikanth <srikanthnehrus@gmail.com>
2020-05-30 14:14:46 +0530

    class

---
+++ classexample.py
@@ -0,0 +1,12 @@
+class Car:
+
+    def __init__(self,brand,carname):
+        self.brand = brand
+        self.carname = carname
+
+
+car1 = Car("Mercedes Benz","Coupe")
+car2 = Car("Audi","A6")
+
+print(car1.brand)
+print(car2.brand)
